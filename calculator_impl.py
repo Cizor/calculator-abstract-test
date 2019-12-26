83d9e67cef211c174b33930ac1169e8571d40c65
AmritTiVo <amrit.srivastava@tivo.com>
2019-12-26 12:08:15 +0530

    Overriding sub and adding test line

--- calculator_impl.py
+++ calculator_impl.py
@@ -2,8 +2,14 @@ from calculator import Calculator
 
 class CalculatorImpl(Calculator):
 
+    def sub(self, a, b):
+        return a - b
+
 
 
 # Test main
 if __name__ == '__main__':
     c = CalculatorImpl()
+
+    #Test line for sub here
+    c.sub(8, 3)
