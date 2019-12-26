f96cbe7bd7b912e0ba43549dcaafbd40f199bc3d
AmritTiVo <amrit.srivastava@tivo.com>
2019-12-26 12:10:16 +0530

    Overriding mul and adding test line

--- calculator_impl.py
+++ calculator_impl.py
@@ -2,8 +2,14 @@ from calculator import Calculator
 
 class CalculatorImpl(Calculator):
 
+    def mul(self, a, b):
+        return a * b
+
 
 
 # Test main
 if __name__ == '__main__':
     c = CalculatorImpl()
+
+    # Test line for mul here
+    print(c.mul(3, 6))
