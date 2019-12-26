ee438e68291938a3130a85df4a78db60ede547ee
AmritTiVo <amrit.srivastava@tivo.com>
2019-12-26 12:12:17 +0530

    Overriding div and adding test line

--- calculator_impl.py
+++ calculator_impl.py
@@ -2,8 +2,15 @@ from calculator import Calculator
 
 class CalculatorImpl(Calculator):
 
+    def div(self, a, b):
+        return a / b
+    
+
 
 
 # Test main
 if __name__ == '__main__':
     c = CalculatorImpl()
+
+    # Test line for div here
+    print(c.div(4, 2))
