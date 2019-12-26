4f67799efc13c7d2bcbaf17c58f7b79abc385e10
AmritTiVo <amrit.srivastava@tivo.com>
2019-12-26 11:55:24 +0530

    Commiting calculator interface of techlead

---
+++ calculator.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+
+class Calculator(ABC):
+
+    @abstractmethod
+    def sum(self, a, b):
+        pass
+
+    @abstractmethod
+    def sub(self, a, b):
+        pass
+
+    @abstractmethod
+    def mul(self, a, b):
+        pass
+
+    @abstractmethod
+    def div(self, a, b):
+        pass
+
